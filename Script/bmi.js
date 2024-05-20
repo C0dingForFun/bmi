887823532fc261ffd917ff804d75614b4b212a03
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-05-20 11:50:33 +0200

    Done

--- Script/bmi.js
+++ Script/bmi.js
@@ -5,5 +5,19 @@ let calc = document.querySelector('.answer');
 
 calc.addEventListener('click', ()=>{
     span.innerText = parseFloat(eval(`${weight.value} / (${height.value/100})**2`)).toFixed(2);
+
+    if (span.innerText <= 18.4){
+        span.innerText = span.innerText + " - Underweight";
+    }
+    else if(span.innerText >= 18.5 && span.innerText <= 24.9 ) {
+        span.innerText = span.innerText + " - Normal";
+    }
+    else if(span.innerText >= 25.0 && span.innerText <= 39.9 ) {
+        span.innerText = span.innerText + " - Overweight";
+    }
+    else{
+        span.innerText = span.innerText + " - Obese";
+    }
+
 })
 
